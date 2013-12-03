6f5bdd12f2fb2a7c10cf8f38bc2cddd1f6e0b0ed
aysekucuk <ayse.kucukk@gmail.com>
2013-12-03 23:42:25 +0200

    new

--- testApp/models.py
+++ testApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __unicode__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add =True)
+    category = models.ForeignKey(Category)
+
+    def __unicode__(self):
+        return self.content
+
--- testApp/views.py
+++ testApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from testApp.models import *
 
 # Create your views here.
+def contents(request):
+    blogs = Blog.objects.all()
+
+    return render(request,"home.html",{"blogs":blogs})
