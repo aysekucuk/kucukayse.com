e6ef7b93c217dcc94b656c960c698b9ff0f8dffb
Mert Öksüz <xrefresh@gmail.com>
2013-12-04 23:15:02 +0200

    aasd

--- testProject/urls.py
+++ testProject/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    #url(r'^.*$', RedirectView.as_view(url='<url_to_home_view>', permanent=False), name='index'),
     url(r'^$', 'testApp.views.contents', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
