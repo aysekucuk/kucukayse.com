7b5edfe0c8d9082c97afb1bd303ac2495491771d
aysekucuk <ayse.kucukk@gmail.com>
2013-12-06 11:11:07 +0200

    slug fields change to read_only

--- testApp/admin.py
+++ testApp/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from testApp.models import *
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ("title","content","date","category")
+	list_display = ("title","content","date","category")
+	readonly_fields = ('slug',)
+
+class CategoryAdmin(admin.ModelAdmin):
+	readonly_fields = ('slug',)
+
+class MainMenuAdmin(admin.ModelAdmin):
+	readonly_fields = ('slug',)
+
 
 admin.site.register(Blog,BlogAdmin)
-admin.site.register(Category)
-admin.site.register(MainMenu)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(MainMenu, MainMenuAdmin)
 admin.site.register(Comment)
 admin.site.register(Tag)
