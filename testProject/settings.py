e1d00ce0f12896eb6fac1814f65814fea2dab9e4
aysekucuk <ayse.kucukk@gmail.com>
2013-12-21 07:05:50 -0500

    settings

--- testProject/settings.py
+++ testProject/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'testProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hello',
+	'USER': 'root',
+	'PASSWORD':'1',
+	'HOST':'localhost',
+	'PORT':'',
     }
 }
 TEMPLATE_CONTEXT_PROCESSORS = TCP + (
