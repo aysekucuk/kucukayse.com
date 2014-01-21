f66ae5512960b72c7cc3ff86a484d82d3ff010f5
aysekucuk <ayse.kucukk@gmail.com>
2014-01-21 11:38:39 +0200

    fot

--- testApp/templatetags/footer.py
+++ testApp/templatetags/footer.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.inclusion_tag("footer.html",takes_context=True)
 def footer(context):
 	context['tags'] = Tag.objects.all()[0:20]
-	context['recents'] = Blog.objects.filter(status=True).order_by('-date')[0:5]
+	context['recents'] = Blog.objects.filter(is_active=True).order_by('-date')[0:5]
 
 	return context
 
--- testProject/settings.py
+++ testProject/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '36*3xk^+q%5*v7(%vnf(&1ygvcm48a96&2spy6%drxj(!4@$46'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 THUMBNAIL_DEBUG = True
@@ -90,7 +90,7 @@ USE_L10N = True
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT =BASE_DIR
 STATIC_URL = "/static/"
 MEDIA_ROOT =BASE_DIR
 MEDIA_URL = "/"
