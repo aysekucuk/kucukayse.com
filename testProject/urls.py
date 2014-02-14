e9407f2bead2bcc3a09be5c9f8675694080922f4
aysekucuk <ayse.kucukk@gmail.com>
2014-02-14 15:31:49 +0200

    url change

--- testProject/urls.py
+++ testProject/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 from testApp.templatetags import menu
 import settings
 
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
